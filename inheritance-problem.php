37e16de693d1c377da1934b9f860e8b7e414a76c
fahrisyabani <macbook@Yogis-MacBook-Air.local>
2021-09-24 16:00:54 +0700

    membuat file inheritance yang berisi parent & child. child class, mewarisi semua properti dan method dari parent-nya

--- inheritance-problem.php
+++ inheritance-problem.php
@@ -7,8 +7,7 @@ class Produk
         $penerbit,
         $harga,
         $jmlHalaman,
-        $waktuMain,
-        $tipe;
+        $waktuMain;
 
     // method akan otomatis dijalankan setiap melakukan intansiasi kelas
     // constractor akan menerima parameter/isi dari class Produk
@@ -18,8 +17,8 @@ class Produk
         $penerbit = "penerbit",
         $harga = 0,
         $jmlHalaman = 0,
-        $waktuMain = 0,
-        $tipe
+        $waktuMain = 0
+
     ) {
         // this->$judul diambil dari class Produk
         // dan $judul untuk memanggil dari constractor
@@ -29,7 +28,6 @@ class Produk
         $this->harga = $harga;
         $this->jmlHalaman = $jmlHalaman;
         $this->waktuMain = $waktuMain;
-        $this->tipe = $tipe;
     }
 
     public function getLabel()
@@ -37,17 +35,29 @@ class Produk
         return "$this->penulis, $this->penerbit";
     }
 
-    public function getInfoLengkap()
+    public function getInfoProduk()
     {
-        // Komik : Naruto | Masashi Kishimoto, Shonen Jump (Rp. 30000) - 100 Halaman.
-        $str = "{$this->tipe} : {$this->judul} | {$this->getLabel()} (Rp. {$this->harga})";
+        $str = "{$this->judul} | {$this->getLabel()} (Rp. {$this->harga})";
 
-        if ($this->tipe == "Komik") {
-            $str .= " - {$this->jmlHalaman} Halaman.";
-        } else if ($this->tipe == "Game") {
-            $str .= " ~ {$this->waktuMain} Jam. ";
-        }
+        return $str;
+    }
+}
 
+
+class Komik extends Produk
+{
+    public function getInfoProduk()
+    {
+        $str = "Komik : {$this->judul} | {$this->getLabel()} (Rp. {$this->harga}) - {$this->jmlHalaman} Halaman.";
+        return $str;
+    }
+}
+
+class Game extends Produk
+{
+    public function getInfoProduk()
+    {
+        $str = "Game : {$this->judul} | {$this->getLabel()} (Rp. {$this->harga}) - {$this->waktuMain} Jam.";
         return $str;
     }
 }
@@ -63,10 +73,10 @@ class CetakInfoProduk
 }
 
 // isi dari parameter disini akan dikirim kepada constractor
-$produk1 = new Produk("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100, 0, "Komik");
-$produk2 = new Produk("Uncharted", "Neil Druckmann", "Sony Computer", 25000, 0, 50, "Game");
+$produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100, 0,);
+$produk2 = new Game("Uncharted", "Neil Druckmann", "Sony Computer", 25000, 0, 50,);
 
 
-echo $produk1->getInfoLengkap();
+echo $produk1->getInfoProduk();
 echo "<br>";
-echo $produk2->getInfoLengkap();
+echo $produk2->getInfoProduk();
